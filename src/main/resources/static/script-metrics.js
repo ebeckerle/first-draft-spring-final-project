bb757b595e7820ffedb8276a2cd4958af9479ecb
Elizabeth <92605769+ebeckerle@users.noreply.github.com>
2022-09-16 17:43:56 -0500

    "JavaScript front-end of supervisorDashboard"

--- src/main/resources/static/script-metrics.js
+++ src/main/resources/static/script-metrics.js
@@ -1,39 +1,7 @@
 function runCharts(){
 
     let chartDataElement = document.querySelector("#chart");
-         let chartData = chartDataElement.getAttribute("xyValues").split(",");
-
-
-// CODE TO CONVERT A HASH MAP TO CREATE AN OBJECT FOR A SCATTER PLOT
-//        let xyValues = [];
-//         for(let valueSet in chartData){
-//            let xy = chartData[valueSet].replace("{","").replace("}", "");
-//            let xyArray = xy.split("=");
-//            let xValue = xyArray[0];
-//            console.log(xValue);
-//            let yValue = xyArray[1];
-//            console.log(yValue);
-//            let xyPair = {x:xValue, y:yValue};
-//            xyValues.push(xyPair);
-//         }
-
-//        new Chart("myChart", {
-//          type: "scatter",
-//          data: {
-//            datasets: [{
-//              pointRadius: 4,
-//              pointBackgroundColor: "rgb(0,0,255)",
-//              data: xyValues
-//            }]
-//          },
-//          options: {
-//            legend: {display: false},
-//            scales: {
-//              xAxes: [{ticks: {min: 40, max:160}}],
-//              yAxes: [{ticks: {min: 6, max:16}}],
-//            }
-//          }
-//        });
+    let chartData = chartDataElement.getAttribute("xyValues").split(",");
 
 }
 
--- src/main/resources/static/supervisorDashScript.js
+++ src/main/resources/static/supervisorDashScript.js
@@ -1,9 +1,21 @@
-runFunction();
+//runFunction();
 function runFunction(){
-    let textElementArray = document.querySelectorAll('.trial');
+    console.log("fox");
+
+    let project1 = document.getElementById("1");
+    project1.style.backgroundColor = "#245761";
+    let project2 = document.getElementById("2");
+    project2.style.backgroundColor = "#3B90A1";
+    let project3 = document.getElementById("3");
+    project3.style.backgroundColor = "#52C8E0";
+    let project4 = document.getElementById("4");
+    project4.style.backgroundColor = "#4AB2C7";
+
     console.log("hey yo!");
-    textElementArray.forEach(element =>{
-        element.style = `color: #245761`;
-    });
 
 }
+
+window.addEventListener('load', function(){
+    console.log("inside window event listener, on load");
+    runFunction();
+});
