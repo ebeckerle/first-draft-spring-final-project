813ba0a216f7d74ea4960dbec418574810eb2081
Elizabeth <92605769+ebeckerle@users.noreply.github.com>
2023-01-10 11:20:20 -0600

    "1/10 - Manage Contact sub-Feature"

---
+++ src/main/resources/static/script-contacts.js
@@ -0,0 +1,14 @@
+function viewSecondEmailForm(){
+
+//    if (document.getElementById("newCarrier").selected){
+        document.getElementById("secondEmail").style.display = "block";
+//    }
+
+}
+
+
+function viewSecondPhoneNumberForm(){
+
+        document.getElementById("secondPhoneNumber").style.display = "block";
+
+}
