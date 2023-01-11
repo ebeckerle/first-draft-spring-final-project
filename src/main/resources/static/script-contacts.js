6f7f146ba03e3b062abbca77d7c27de1e0a7ac54
Elizabeth <92605769+ebeckerle@users.noreply.github.com>
2023-01-11 14:52:05 -0600

    "1/11 - Manage Contact sub-Feature"

--- src/main/resources/static/script-contacts.js
+++ src/main/resources/static/script-contacts.js
@@ -1,14 +1,16 @@
 function viewSecondEmailForm(){
 console.log("in the view second email form")
-//    if (document.getElementById("newCarrier").selected){
+    if (document.getElementById("addEmail").checked){
         document.getElementById("secondEmail").style.display = "block";
-//    }
+    }
 
 }
 
 
 function viewSecondPhoneNumberForm(){
 
+    if (document.getElementById("addPhoneNumber").checked){
         document.getElementById("secondPhoneNumber").style.display = "block";
+    }
 
 }
