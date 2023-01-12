56ac66419f32042acfbbc55897da0ad027647cb8
Elizabeth <92605769+ebeckerle@users.noreply.github.com>
2023-01-12 10:49:51 -0600

    "1/12 - Manage Contact sub-Feature - add contact form"

--- src/main/resources/static/script-contacts.js
+++ src/main/resources/static/script-contacts.js
@@ -1,16 +1,52 @@
-function viewSecondEmailForm(){
-console.log("in the view second email form")
-    if (document.getElementById("addEmail").checked){
-        document.getElementById("secondEmail").style.display = "block";
-    }
 
+//TODO - DRY out - these 6 functions could maybe be 3?? - click and unclick radio button to show a second form input
+let radioStateEmail = false;
+let radioStatePhoneNumber = false;
+function viewSecondEmailForm(){
+
+    if(radioStateEmail == false) {
+        checkRadioButtonEmail();
+        radioStateEmail = true;
+        document.getElementById("secondEmail").style.display = "block";
+    }else{
+        uncheckRadioButtonEmail();
+        radioStateEmail = false;
+        document.getElementById("secondEmail").style.display = "none";
+        //TODO - if button is filled with data when the radio button is unchecked then the data disappears
+    }
 }
 
 
 function viewSecondPhoneNumberForm(){
 
+        if(radioStatePhoneNumber == false) {
+            checkRadioButtonPhoneNumber();
+            radioStatePhoneNumber = true;
+            document.getElementById("secondPhoneNumber").style.display = "block";
+        }else{
+            uncheckRadioButtonPhoneNumber();
+            radioStatePhoneNumber = false;
+            document.getElementById("secondPhoneNumber").style.display = "none";
+            //TODO - if button is filled with data when the radio button is unchecked then the data disappears
+        }
     if (document.getElementById("addPhoneNumber").checked){
         document.getElementById("secondPhoneNumber").style.display = "block";
     }
 
+}
+
+function checkRadioButtonEmail(){
+    document.getElementById("addEmail").checked = true;
+}
+
+function uncheckRadioButtonEmail(){
+    document.getElementById("addEmail").checked = false;
+}
+
+function checkRadioButtonPhoneNumber(){
+    document.getElementById("addPhoneNumber").checked = true;
+}
+
+function uncheckRadioButtonPhoneNumber(){
+    document.getElementById("addPhoneNumber").checked = false;
 }
