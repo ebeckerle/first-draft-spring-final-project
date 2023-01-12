17f3de55a3ec8b83558a668293374b634ced8686
Elizabeth <92605769+ebeckerle@users.noreply.github.com>
2023-01-12 12:56:36 -0600

    "1/12 - Manage Contact sub-Feature - add contact form"

--- src/main/java/com/example/firstdraftspringfinalproject/models/interfaces/ContactConstants.java
+++ src/main/java/com/example/firstdraftspringfinalproject/models/interfaces/ContactConstants.java
@@ -9,6 +9,14 @@ public interface ContactConstants {
         String AK = "AK";
         String AZ = "AZ";
         String AR = "AR";
+        String CA = "CA";
+        String CO = "CO";
+        String CT = "CT";
+        String DE = "DE";
+        String FL = "FL";
+        String GA = "GA";
+        String HI = "HI";
+        String ID = "ID";
         ArrayList<String> allStatesPostalCodes = new ArrayList<>();
         allStatesPostalCodes.add(AL);
         allStatesPostalCodes.add(AK);
