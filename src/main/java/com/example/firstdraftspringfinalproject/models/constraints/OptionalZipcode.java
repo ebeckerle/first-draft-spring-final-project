e02e6fa2adc518c5d4f331ebc6122893e1dc0b92
Elizabeth <92605769+ebeckerle@users.noreply.github.com>
2023-01-18 10:08:37 -0600

    "1/18 - contact subfeature - custom constraints - back-end validation"

--- src/main/java/com/example/firstdraftspringfinalproject/models/constraints/OptionalZipcode.java
+++ src/main/java/com/example/firstdraftspringfinalproject/models/constraints/OptionalZipcode.java
@@ -1,4 +1,10 @@
 package com.example.firstdraftspringfinalproject.models.constraints;
 
 public @interface OptionalZipcode {
+
+    String message() default "{com.example.firstdraftspringfinalproject.models.constraints.OptionalZipcode." +
+            "message}";
+
+    Class<?>[] groups() default { };
+
 }
