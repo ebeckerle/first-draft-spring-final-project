f3d5f655aa101435cff103309b2abaf29cf6d823
ebeckerle <92605769+ebeckerle@users.noreply.github.com>
2023-04-05 10:04:22 -0500

    "4/5"

--- src/main/java/com/example/firstdraftspringfinalproject/models/interfaces/ContactConstants.java
+++ src/main/java/com/example/firstdraftspringfinalproject/models/interfaces/ContactConstants.java
@@ -6,14 +6,6 @@ import java.util.HashMap;
 public interface ContactConstants {
 
     static HashMap<String, String> populateAllStatesHashMap(){
-        String CA = "CA";
-        String CO = "CO";
-        String CT = "CT";
-        String DE = "DE";
-        String FL = "FL";
-        String GA = "GA";
-        String HI = "HI";
-        String ID = "ID";
         HashMap<String, String> allStatesPostalCodes = new HashMap<>();
         allStatesPostalCodes.put("AL", "Alabama");
         allStatesPostalCodes.put("AK", "Alaska");
@@ -21,12 +13,12 @@ public interface ContactConstants {
         allStatesPostalCodes.put("AR", "Arkansas");
         allStatesPostalCodes.put("CA", "California");
         allStatesPostalCodes.put("CO", "Colorado");
-        allStatesPostalCodes.put(CT, "Connecticut");
-        allStatesPostalCodes.put(DE, "Delaware");
-        allStatesPostalCodes.put(FL, "Florida");
-        allStatesPostalCodes.put(GA, "Georgia");
-        allStatesPostalCodes.put(HI, "Hawaii");
-        allStatesPostalCodes.put(ID, "Idaho");
+        allStatesPostalCodes.put("CT", "Connecticut");
+        allStatesPostalCodes.put("DE", "Delaware");
+        allStatesPostalCodes.put("FL", "Florida");
+        allStatesPostalCodes.put("GA", "Georgia");
+        allStatesPostalCodes.put("HI", "Hawaii");
+        allStatesPostalCodes.put("ID", "Idaho");
         allStatesPostalCodes.put("IL", "Illinois");
         allStatesPostalCodes.put("IN", "Indiana");
         allStatesPostalCodes.put("IA", "Iowa");
