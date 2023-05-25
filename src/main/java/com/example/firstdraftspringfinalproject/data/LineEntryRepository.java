088088c682b5abd57ad4c5b4dfdc4f8cc10ceadc
ebeckerle <92605769+ebeckerle@users.noreply.github.com>
2023-05-25 18:51:19 -0500

    "5/25 - native queries for charts"

--- src/main/java/com/example/firstdraftspringfinalproject/data/LineEntryRepository.java
+++ src/main/java/com/example/firstdraftspringfinalproject/data/LineEntryRepository.java
@@ -14,5 +14,10 @@ public interface LineEntryRepository extends CrudRepository<LineEntry, Integer> 
             nativeQuery = true)
     List<String> findAllHoursByProject();
 
+    @Query(value = "SELECT a.work_description, b.total_hours FROM work_type a, line_entry b " +
+            "WHERE b.timesheet_id = (SELECT id FROM timesheet WHERE supervisor_approval = true) AND b.work_type_id = a.id",
+            nativeQuery = true)
+    List<String> findAllHoursByWorkType();
+
 
 }
