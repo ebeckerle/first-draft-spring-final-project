2d861886a6f0474782e140d4164a7e9fd8c2d2a7
Elizabeth <92605769+ebeckerle@users.noreply.github.com>
2023-06-08 18:54:44 -0500

    "6/8 - improving MetricsChart feature"

--- src/test/java/com/example/firstdraftspringfinalproject/modelstests/MetricsChartTest.java
+++ src/test/java/com/example/firstdraftspringfinalproject/modelstests/MetricsChartTest.java
@@ -1,9 +1,6 @@
 package com.example.firstdraftspringfinalproject.modelstests;
 
-import com.example.firstdraftspringfinalproject.data.EmployeeRepository;
-import com.example.firstdraftspringfinalproject.data.ProjectRepository;
-import com.example.firstdraftspringfinalproject.data.TimesheetRepository;
-import com.example.firstdraftspringfinalproject.data.WorkTypeRepository;
+import com.example.firstdraftspringfinalproject.data.*;
 import com.example.firstdraftspringfinalproject.models.dao.Chart;
 import com.example.firstdraftspringfinalproject.models.dao.metrics.PrimaryMetricChart;
 import com.example.firstdraftspringfinalproject.models.dao.metrics.SecondaryMetricChart;
@@ -28,6 +25,9 @@ public class MetricsChartTest {
     @Autowired
     WorkTypeRepository workTypeRepository;
 
+    @Autowired
+    LineEntryRepository lineEntryRepository;
+
 //    Metrics testMetricsObj1 = new Metrics("Employee", employeeRepository, timesheetRepository, projectRepository, workTypeRepository);
 
     Chart testMetricsObj2 = new SecondaryMetricChart(MetricsCategory.PROJECT, "Notre Dame Cathedral", MetricsCategory.WORKTYPE, employeeRepository, timesheetRepository, projectRepository, workTypeRepository);
@@ -41,7 +41,8 @@ public class MetricsChartTest {
     @Test
     public void testSetXyValuesWhenThereIsNoSecondaryCategory(){
 
-        Chart testMetricsObj1 = new PrimaryMetricChart(MetricsCategory.EMPLOYEE, employeeRepository, timesheetRepository, projectRepository, workTypeRepository);
+//        Chart testMetricsObj1 = new PrimaryMetricChart(MetricsCategory.EMPLOYEE, employeeRepository, timesheetRepository, projectRepository, workTypeRepository);
+        Chart testMetricsObj1 = new PrimaryMetricChart(MetricsCategory.EMPLOYEE, employeeRepository, lineEntryRepository);
         testMetricsObj1.populateChartData();
 
         assertFalse(testMetricsObj1.getXyValues().isEmpty());
