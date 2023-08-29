b717b6cd4b72964813c88fef57d936ba71b9a449
Elizabeth <92605769+ebeckerle@users.noreply.github.com>
2023-08-29 14:59:05 -0500

    "8/29 - Metrics fixes, refactoring to builder pattern"

--- src/main/java/com/example/firstdraftspringfinalproject/models/dao/metrics/SecondaryMetricChart.java
+++ src/main/java/com/example/firstdraftspringfinalproject/models/dao/metrics/SecondaryMetricChart.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
-public class SecondaryMetricChart extends Chart implements MetricsPayRate, MetricsWorkType, MetricsProject, MetricsEmployee {
+public class SecondaryMetricChart extends Chart {
 
     @Autowired
     private EmployeeRepository employeeRepository;
