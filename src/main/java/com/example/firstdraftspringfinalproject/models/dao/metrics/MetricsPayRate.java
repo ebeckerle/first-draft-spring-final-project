39108d3fd5a49e7ae8190ad2c21cb008d44b5832
Elizabeth <92605769+ebeckerle@users.noreply.github.com>
2023-09-05 14:27:03 -0500

    "9/2 - cleaning up Metrics Feature post refactoring"

--- src/main/java/com/example/firstdraftspringfinalproject/models/dao/metrics/MetricsPayRate.java
+++ src/main/java/com/example/firstdraftspringfinalproject/models/dao/metrics/MetricsPayRate.java
@@ -1,91 +1,91 @@
-package com.example.firstdraftspringfinalproject.models.dao.metrics;
-
-import com.example.firstdraftspringfinalproject.data.TimesheetRepository;
-import com.example.firstdraftspringfinalproject.models.domainentityclasses.Project;
-import com.example.firstdraftspringfinalproject.models.domainentityclasses.timesheets.LineEntry;
-import com.example.firstdraftspringfinalproject.models.domainentityclasses.timesheets.Timesheet;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-public interface MetricsPayRate {
-
-    static HashMap<String, Integer> loadXyValuesForPrimaryCategoryPayRate(TimesheetRepository timesheetRepository){
-        HashMap<String, Integer> xyValues = new HashMap<>();
-        List<Integer> payRates = new ArrayList<>();
-        for (Timesheet timesheet:
-                timesheetRepository.findBySupervisorApprovalAndCompletionStatus(true, true)) {
-            Integer payRate = timesheet.getCurrentPayRate();
-            if(payRates.contains(payRate)){
-                Integer existingHourTotal = xyValues.get(String.valueOf(payRate));
-                Integer newHourTotal = existingHourTotal + timesheet.getTotalHours();
-                xyValues.replace(String.valueOf(payRate), newHourTotal);
-            }else{
-                payRates.add(payRate);
-                xyValues.put(String.valueOf(payRate), timesheet.getTotalHours());
-            }
-        }
-        return xyValues;
-    }
-
-    static HashMap<String, Integer> loadXyValuesForSecondaryCategoryPayRateWhenPrimaryCategoryIsProject(List<Timesheet> timesheets,
-                                                                                                                Project project) {
-        HashMap<String, Integer> xyValues = new HashMap<>();
-        List<String> payRates = new ArrayList<>();
-        for (Timesheet timesheet :
-                timesheets) {
-            List<LineEntry> lineEntries = timesheet.getLineEntries();
-            for (LineEntry lineEntry :
-                    lineEntries) {
-                if (lineEntry.getProject().equals(project)) {
-                    String payRate = "$" + timesheet.getCurrentPayRate() + " per hour";
-                    if (payRates.contains(payRate)) {
-                        Integer existingHourTotal = xyValues.get(payRate);
-                        Integer newHourTotal = existingHourTotal + lineEntry.getTotalHours();
-                        xyValues.replace(payRate, newHourTotal);
-                    } else {
-                        payRates.add(payRate);
-                        xyValues.put(payRate, lineEntry.getTotalHours());
-                    }
-                }
-            }
-        }
-        return xyValues;
-    }
-
-    static HashMap<String, Integer> loadXyValuesForSecondaryCategoryPayRateWhenPrimaryCategoryIsWorkType(TimesheetRepository timesheetRepository, String workTypeName) {
-        HashMap<String, Integer> xyValues = new HashMap<>();
-        for (Timesheet timesheet :
-                timesheetRepository.findBySupervisorApprovalAndCompletionStatus(true, true)) {
-            for (LineEntry lineEntry :
-                    timesheet.getLineEntries()) {
-                String workType1 = lineEntry.getWorkType().toString();
-                if (workTypeName.equals(workType1)) {
-                    String payRate1 = "$" + timesheet.getCurrentPayRate() + " per hour";
-                    if (xyValues.containsKey(payRate1)) {
-                        Integer existingHourTotal = xyValues.get(payRate1);
-                        Integer newHourTotal = existingHourTotal + lineEntry.getTotalHours();
-                        xyValues.replace(payRate1, newHourTotal);
-                    } else {
-                        xyValues.put(payRate1, lineEntry.getTotalHours());
-                    }
-                }
-            }
-        }
-        return xyValues;
-    }
-
-    static List<Integer> loadListOfPayRates(List<Timesheet> timesheets){
-        ArrayList<Integer> payRates = new ArrayList<>();
-        for (Timesheet timesheet:
-                timesheets) {
-            if (!payRates.contains(timesheet.getCurrentPayRate())){
-                payRates.add(timesheet.getCurrentPayRate());
-            }
-        }
-        return payRates;
-    }
-
-
-}
+//package com.example.firstdraftspringfinalproject.models.dao.metrics;
+//
+//import com.example.firstdraftspringfinalproject.data.TimesheetRepository;
+//import com.example.firstdraftspringfinalproject.models.domainentityclasses.Project;
+//import com.example.firstdraftspringfinalproject.models.domainentityclasses.timesheets.LineEntry;
+//import com.example.firstdraftspringfinalproject.models.domainentityclasses.timesheets.Timesheet;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//
+//public interface MetricsPayRate {
+//
+//    static HashMap<String, Integer> loadXyValuesForPrimaryCategoryPayRate(TimesheetRepository timesheetRepository){
+//        HashMap<String, Integer> xyValues = new HashMap<>();
+//        List<Integer> payRates = new ArrayList<>();
+//        for (Timesheet timesheet:
+//                timesheetRepository.findBySupervisorApprovalAndCompletionStatus(true, true)) {
+//            Integer payRate = timesheet.getCurrentPayRate();
+//            if(payRates.contains(payRate)){
+//                Integer existingHourTotal = xyValues.get(String.valueOf(payRate));
+//                Integer newHourTotal = existingHourTotal + timesheet.getTotalHours();
+//                xyValues.replace(String.valueOf(payRate), newHourTotal);
+//            }else{
+//                payRates.add(payRate);
+//                xyValues.put(String.valueOf(payRate), timesheet.getTotalHours());
+//            }
+//        }
+//        return xyValues;
+//    }
+//
+//    static HashMap<String, Integer> loadXyValuesForSecondaryCategoryPayRateWhenPrimaryCategoryIsProject(List<Timesheet> timesheets,
+//                                                                                                                Project project) {
+//        HashMap<String, Integer> xyValues = new HashMap<>();
+//        List<String> payRates = new ArrayList<>();
+//        for (Timesheet timesheet :
+//                timesheets) {
+//            List<LineEntry> lineEntries = timesheet.getLineEntries();
+//            for (LineEntry lineEntry :
+//                    lineEntries) {
+//                if (lineEntry.getProject().equals(project)) {
+//                    String payRate = "$" + timesheet.getCurrentPayRate() + " per hour";
+//                    if (payRates.contains(payRate)) {
+//                        Integer existingHourTotal = xyValues.get(payRate);
+//                        Integer newHourTotal = existingHourTotal + lineEntry.getTotalHours();
+//                        xyValues.replace(payRate, newHourTotal);
+//                    } else {
+//                        payRates.add(payRate);
+//                        xyValues.put(payRate, lineEntry.getTotalHours());
+//                    }
+//                }
+//            }
+//        }
+//        return xyValues;
+//    }
+//
+//    static HashMap<String, Integer> loadXyValuesForSecondaryCategoryPayRateWhenPrimaryCategoryIsWorkType(TimesheetRepository timesheetRepository, String workTypeName) {
+//        HashMap<String, Integer> xyValues = new HashMap<>();
+//        for (Timesheet timesheet :
+//                timesheetRepository.findBySupervisorApprovalAndCompletionStatus(true, true)) {
+//            for (LineEntry lineEntry :
+//                    timesheet.getLineEntries()) {
+//                String workType1 = lineEntry.getWorkType().toString();
+//                if (workTypeName.equals(workType1)) {
+//                    String payRate1 = "$" + timesheet.getCurrentPayRate() + " per hour";
+//                    if (xyValues.containsKey(payRate1)) {
+//                        Integer existingHourTotal = xyValues.get(payRate1);
+//                        Integer newHourTotal = existingHourTotal + lineEntry.getTotalHours();
+//                        xyValues.replace(payRate1, newHourTotal);
+//                    } else {
+//                        xyValues.put(payRate1, lineEntry.getTotalHours());
+//                    }
+//                }
+//            }
+//        }
+//        return xyValues;
+//    }
+//
+//    static List<Integer> loadListOfPayRates(List<Timesheet> timesheets){
+//        ArrayList<Integer> payRates = new ArrayList<>();
+//        for (Timesheet timesheet:
+//                timesheets) {
+//            if (!payRates.contains(timesheet.getCurrentPayRate())){
+//                payRates.add(timesheet.getCurrentPayRate());
+//            }
+//        }
+//        return payRates;
+//    }
+//
+//
+//}
