22442425fe01ea046a12fac25aba5c800d21a50e
Elizabeth <92605769+ebeckerle@users.noreply.github.com>
2023-09-06 15:01:23 -0500

    "9/6 - fixing bugs in employee portal - timesheet display"

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -8,14 +8,14 @@ window.addEventListener('load', function(){
     if(document.title == "Metrics"){
         viewBarGraph();
     }
-    if(document.title == "Timesheet"){
+    if(document.title == "Current Timesheet"){
         let form = document.getElementById("create-line-entry");
         form.addEventListener("submit", function(event){
         let hours = document.querySelector("input[name=hours]");
-            if (hours.value >= 18 || hours.value <= 0){
-                alert("You can only work between 1 to 18 hours");
-                event.preventDefault();
-            }
+        if (hours.value >= 18 || hours.value <= 0){
+            alert("You can only work between 1 to 18 hours");
+            event.preventDefault();
+        }
         });
     }
     if(document.title == "Manage Shipments"){
