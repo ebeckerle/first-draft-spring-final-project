6ba6ddc40da08891fd10dbb928dc65cc8e220e3d
Elizabeth <92605769+ebeckerle@users.noreply.github.com>
2023-09-08 14:04:16 -0500

    "9/8 - fixing up Timesheet Unit tests"

--- src/test/java/com/example/firstdraftspringfinalproject/unittests/MetricsChartTest.java
+++ src/test/java/com/example/firstdraftspringfinalproject/unittests/MetricsChartTest.java
@@ -13,24 +13,6 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MetricsChartTest {
 
 
-    @Autowired
-    EmployeeRepository employeeRepository;
-
-    @Autowired
-    TimesheetRepository timesheetRepository;
-
-    @Autowired
-    ProjectRepository projectRepository;
-
-    @Autowired
-    WorkTypeRepository workTypeRepository;
-
-    @Autowired
-    LineEntryRepository lineEntryRepository;
-
-//    Metrics testMetricsObj1 = new Metrics("Employee", employeeRepository, timesheetRepository, projectRepository, workTypeRepository);
-
-//    Chart testMetricsObj2 = new SecondaryMetricChart(MetricsCategory.PROJECT, "Notre Dame Cathedral", MetricsCategory.WORKTYPE, employeeRepository, timesheetRepository, projectRepository, workTypeRepository);
 
     @Test
     public void test(){
@@ -38,14 +20,5 @@ public class MetricsChartTest {
         assertEquals("101", "102");
     }
 
-    @Test
-    public void testSetXyValuesWhenThereIsNoSecondaryCategory(){
 
-//        Chart testMetricsObj1 = new PrimaryMetricChart(MetricsCategory.EMPLOYEE, employeeRepository, timesheetRepository, projectRepository, workTypeRepository);
-        Chart testMetricsObj1 = new PrimaryMetricChart(MetricsCategory.EMPLOYEE, employeeRepository, lineEntryRepository);
-        testMetricsObj1.populateChartData();
-
-        assertFalse(testMetricsObj1.getXyValues().isEmpty());
-
-    }
 }
